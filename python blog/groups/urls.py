bef2eea1f977d0705730a7d1398014bf54758d2d
Shivank Srivastava <shivank309@gmail.com>
2019-03-26 23:33:50 +0530

    'first'

---
+++ python blog/groups/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path
+
+app_name = 'groups'
+
+"""urlpatterns = [
+    url(r'^$',views.ListGroups.as_view(),name='all'),
+    url(r'^new/$',views.CreateGroup.as_view(),name='create'),
+    url(r'posts/in/(?P<slug>[-\w]+)/$',views.SingleGroup.as_view(),name='single'),
+    url(r'join/(?P<slug>[-\w]+)/$',views.JoinGroup.as_view(),name='join'),
+    url(r'leave/(?P<slug>[-\w]+)/$',views.LeaveGroup.as_view(),name='leave'),
+
+]"""
+
+urlpatterns = [
+    path('',views.ListGroups.as_view(),name='all'),
+    path('new/',views.CreateGroup.as_view(),name='create'),
+    path(r'posts/in/<slug:slug>/',views.SingleGroup.as_view(),name='single'),
+    path('join/<slug:slug>/',views.JoinGroup.as_view(),name='join'),
+    path('leave/<slug:slug>/',views.LeaveGroup.as_view(),name='leave'),
+
+]
---
+++ python blog/posts/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+"""urlpatterns = [
+      url(r'^$',views.PostList.as_view(),name='all'),
+      url(r'new/$',views.CreatePost.as_view(),name='create'),
+      url(r'by/(?P<username>[-\w]+)',views.UserPosts.as_view(),name='for_user'),
+      url(r'by/(?P<username>[-\w]+)/(?P<pk>\d+)/$',views.PostDetail.as_view(),name='single'),
+      url(r'delete/(?P<pk>\d+)/$',views.DeletePost.as_view(),name='delete')
+]"""
+
+urlpatterns = [
+      path('',views.PostList.as_view(),name='all'),
+      path('new/',views.CreatePost.as_view(),name='create'),
+      path('by/<str:username>',views.UserPosts.as_view(),name='for_user'),
+      path('by/<str:username>/<int:pk>/',views.PostDetail.as_view(),name='single'),
+      path('delete/<int:pk>/',views.DeletePost.as_view(),name='delete')
+]
